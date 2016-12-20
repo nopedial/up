b529af470bdb7612b4bd6839a1d03d3291a8eb33
nopedial <sam@arahant.net>
2016-12-20 00:24:29 +0000

    version 0.1.0

--- README.md
+++ README.md
@@ -9,9 +9,9 @@ user@box:~$ cd up/
 user@box:~/up$ rake install
   Successfully built RubyGem
   Name: up
-  Version: 0.0.1
-  File: up-0.0.1.gem
-Successfully installed up-0.0.1
+  Version: 0.1.0
+  File: up-0.1.0.gem
+Successfully installed up-0.1.0
 1 gem installed
 user@box:~/up$ cd ../
 user@box:~$ upd
@@ -23,21 +23,66 @@ user@box:~$
 
 ##### configuration
 
-the configuration file is automatically created during the first run.
+the configuration file is automatically generated during the first run. 
 
 ```
-user@box:~$ upd
+shafez@up:~$ upd
 + base configuration built at: ~/.config/up/config
-user@box:~$ cat ~/.config/up/config
+shafez@up:~$ cat .config/up/config
 ---
-debug: false
-uploads_dir: "/home/shafez/public/uploads"
+debug: true
+app:
+  upload_dir: "/home/shafez/public/uploads"
+  times:
+  - 1
+  - 5
+  - 10
+  - 30
+  - 1h
+  - 1d
+  log: "/home/shafez/.config/up/log"
 web:
   localhost: 127.0.0.1
   localport: 8888
-user@box:~$
+shafez@up:~$
+```
+
+files are stored in sub-directories of the defined 'uploads_dir'. these directories can be configured under 'app/times'. up will create the missing directories.
+
+#### Up doesn't remove files - please setup cron to match your configuration
+
+```
+shafez@up:~$ ls -al public/uploads/
+total 32
+drwxr-xr-x 8 shafez shafez 4096 Dec 19 23:44 .
+drwxr-xr-x 3 shafez shafez 4096 Aug 22 21:49 ..
+drwxr-xr-x 2 shafez shafez 4096 Dec 19 23:54 1
+drwxr-xr-x 2 shafez shafez 4096 Dec 19 22:48 10
+drwxr-xr-x 2 shafez shafez 4096 Dec 19 23:26 1d
+drwxr-xr-x 2 shafez shafez 4096 Dec 19 22:48 1h
+drwxr-xr-x 2 shafez shafez 4096 Dec 19 22:48 30
+drwxr-xr-x 2 shafez shafez 4096 Dec 19 23:55 5
+shafez@up:~$
 ```
 
 setting 'debug' to 'true' will make the daemon run in foreground.
+
+
+#### nginx
+
+nginx can be configured to proxy requests to Up
+
+```
+server {
+	listen 80;
+        listen [::]:80;
+        server_name up.arahant.nl;
+    	access_log /var/log/nginx/up.arahant.nl-access.log;
+    	error_log /var/log/nginx/up.arahant.nl-error.log;
+        location / {
+             proxy_pass http://127.0.0.1:8888;
+        }
+}
+```
 
 live demo: http://up.arahant.nl
--- lib/up/web.rb
+++ lib/up/web.rb
@@ -5,7 +5,9 @@ module Up
   else
     CFG = Asetus.new :name=>'up', :load=>false
     CFG.default.debug 		= true
-    CFG.default.uploads_dir	= '/home/user/public/uploads'
+    CFG.default.app.upload_dir  = '/home/user/public/uploads'
+    CFG.default.app.times       = [ 1, 5, 10, 30, '1h', '1d' ]
+    CFG.default.app.log		= '/home/user/.config/up/log'
     CFG.default.web.localhost 	= '127.0.0.1'
     CFG.default.web.localport	= 8888
     CFG.load
@@ -15,7 +17,13 @@ module Up
       exit 0
     else
       Cfg = CFG.cfg
-      Process.daemon if not Cfg.debug
+      if Cfg.debug
+        target = STDOUT
+        Log = Logger.new target
+      else
+        Log = Logger.new Cfg.app.log
+        Process.daemon
+      end
     end
   end
 
@@ -27,16 +35,35 @@ module Up
     set :public_dir, File.dirname(__FILE__) + '/public'
     set :sessions, false
 
+    def init_env
+      Cfg.app.times.each do |t|
+        if not File.exist?(File.join(Cfg.app.upload_dir, t.to_s))
+          Log.debug [ 'adding missing directory:', File.join(Cfg.app.upload_dir, t.to_s) ].join(' ')
+          system([ 'mkdir', File.join(Cfg.app.upload_dir, t.to_s) ].join(' '))
+        end
+      end
+    end 
+
     def fn_gen filext
       tfn = [*('a'..'z'),*('0'..'9')].shuffle[0,4].join
-      if not Dir.entries(Cfg.uploads_dir).include?([tfn, filext].join('.'))
+      if not Dir.entries(Cfg.app.upload_dir).include?([tfn, filext].join('.'))
         return [tfn, filext].join('.')
       else
         fn_gen
       end
     end
 
+    def time_verify tm
+      if Cfg.app.times.include?(tm.to_i)
+        return true
+      else
+        return false
+      end
+    end
+
     get '/' do
+      init_env
+      @times = Cfg.app.times
       haml :index
     end
 
@@ -47,17 +74,30 @@ module Up
         fna = params['myfile'][:filename].split('.')[-1]
       end
       filename = fn_gen fna.split('.')[-1]
-      File.open([Cfg.uploads_dir, '/'].join + filename, 'w') do |f|
+      if time_verify(params['time'])
+        @time = params['time']
+      else
+        @time = 1
+      end
+      File.open([Cfg.app.upload_dir, '/', @time, '/'].join + filename, 'w') do |f|
         f.write(params['myfile'][:tempfile].read)
       end 
-      redirect File.join('/uploads', filename)
+      redirect File.join('/uploads', @time.to_s, filename)
+    end
+
+    get '/uploads/' do
+      redirect '/'
     end
 
     get '/uploads/:filename' do
+      redirect '/'
+    end
+
+    get '/uploads/:time/:filename' do
       begin
-        puts [ Time.now.to_s, File.join('/uploads', params[:filename]) ].join(' ') if Cfg.debug
-        if Dir.entries([Cfg.uploads_dir, '/'].join).include?(params[:filename])
-          send_file [ [Cfg.uploads_dir, '/'].join, params[:filename] ].join
+        Log.debug [ Time.now.to_s, File.join('/uploads', params[:time], params[:filename]) ].join(' ') if Cfg.debug
+        if Dir.entries([Cfg.app.upload_dir, '/', params[:time], '/'].join).include?(params[:filename])
+          send_file [ [Cfg.app.upload_dir, '/', params[:time], '/'].join, params[:filename] ].join
         else
           @filename = params[:filename]
           haml :error
